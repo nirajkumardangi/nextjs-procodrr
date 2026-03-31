9414176210e7814d30772357a6081ad225846f8a
nirajkumardangi <niraj8825224435@gmail.com>
2026-03-31 20:37:00 +0530

    Refactor Comment and Comments components to use async params and improve structure

--- 01-first-next-app/app/blogs/[blog]/comments/[comment]/page.js
+++ 01-first-next-app/app/blogs/[blog]/comments/[comment]/page.js
@@ -2,12 +2,12 @@ export default async function Comment({ params }) {
   const { blog, comment } = await params;
 
   return (
-    <div>
+    <h2>
       Comment id{" "}
       <i>
         <b>{comment}</b>
       </i>{" "}
       on <b>{blog}</b> page
-    </div>
+    </h2>
   );
 }
--- 01-first-next-app/app/blogs/[blog]/comments/page.js
+++ 01-first-next-app/app/blogs/[blog]/comments/page.js
@@ -1,3 +1,8 @@
-export default function Comments() {
-  return <h2>All Comments</h2>;
+export default async function Comments({ params }) {
+  const { blog } = await params;
+  return (
+    <h2>
+      All Comments on <i>{blog}</i> blog
+    </h2>
+  );
 }
