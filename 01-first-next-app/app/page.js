ddf504b2e9ce0ed664e917ab69f30206b70ab835
nirajkumardangi <niraj8825224435@gmail.com>
2026-03-31 18:11:21 +0530

    Initialize Next.js application with Tailwind CSS setup and implement file routing

---
+++ 01-first-next-app/app/page.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Welcome My First Next App</h1>
+      <ul>
+        <li>
+          <Link href="home">Home</Link>
+        </li>
+        <li>
+          <Link href="about">About</Link>
+        </li>
+        <li>
+          <Link href="contacts">Contacts</Link>
+        </li>
+      </ul>
+    </>
+  );
+}
