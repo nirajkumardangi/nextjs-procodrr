06cd4c815ab7f4d0d0bd10ccb6a24cd5065c4cd7
nirajkumardangi <niraj8825224435@gmail.com>
2026-04-01 21:54:39 +0530

    Pre-generate static pages at build time by using function generateStaticParams()

--- 01-first-next-app/app/blogs/[blog]/page.js
+++ 01-first-next-app/app/blogs/[blog]/page.js
@@ -1,3 +1,4 @@
+// Generate metadata for the page title based on the blog parameter
 export async function generateMetadata({ params }) {
   const { blog } = await params;
   return {
@@ -5,7 +6,20 @@ export async function generateMetadata({ params }) {
   };
 }
 
+// Pre-generate static pages for these blog IDs at build time
+export function generateStaticParams() {
+  return [
+    { blog: "1" },
+    { blog: "2" },
+    { blog: "3" },
+    { blog: "4" },
+    { blog: "5" },
+  ];
+}
+
+// Main blog page component - renders the blog post based on the dynamic [blog] parameter
 export default async function blog({ params, searchParams }) {
   const { blog } = await params;
+  console.log(blog);
   return <h2>Blog Name: {blog}</h2>;
 }
--- 01-first-next-app/app/blogs/page.js
+++ 01-first-next-app/app/blogs/page.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export const metadata = {
   title: "Blogs",
 };
@@ -8,7 +10,19 @@ export default function Blogs() {
       <h2>All Blogs</h2>
       <ul>
         <li>
-          <a>Blog 1</a>
+          <Link href="blogs/1">Blog 1</Link>
+        </li>
+        <li>
+          <Link href="blogs/2">Blog 2</Link>
+        </li>
+        <li>
+          <Link href="blogs/3">Blog 3</Link>
+        </li>
+        <li>
+          <Link href="blogs/4">Blog 4</Link>
+        </li>
+        <li>
+          <Link href="blogs/5">Blog 5</Link>
         </li>
       </ul>
     </>
