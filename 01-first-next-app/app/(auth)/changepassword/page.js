c54264804aac808339a05b374b4e89208ceb90a8
nirajkumardangi <niraj8825224435@gmail.com>
2026-04-01 06:46:50 +0530

    implement route group

---
+++ 01-first-next-app/app/(auth)/changepassword/page.js
@@ -0,0 +1,5 @@
+export default function ChangePassword() {
+  return (
+    <h2>Change Password Page</h2>
+  )
+}
---
+++ 01-first-next-app/app/(auth)/signup/page.js
@@ -0,0 +1,5 @@
+export default function SignUp() {
+  return (
+    <h2>SignUp Page</h2>
+  )
+}
