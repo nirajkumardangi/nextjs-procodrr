b159056a1ea469d39946fe07397c767253e52201
nirajkumardangi <niraj8825224435@gmail.com>
2026-04-04 20:02:15 +0530

    Handling post request

--- 04-route-handling/app/todos/route.js
+++ 04-route-handling/app/todos/route.js
@@ -1,13 +1,22 @@
+import { writeFile } from "fs/promises";
 import todosData from "../../todos.json";
 
 export function GET() {
   return Response.json(todosData);
+}
 
-    // return new Response(JSON.stringify(todosData), {
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   status: 200,
-    //   statusText: "ProCodrr",
-    // });
+export async function POST(request) {
+  const todo = await request.json();
+
+  const newTodo = {
+    id: crypto.randomUUID(),
+    title: todo.title,
+    completed: false,
+  };
+
+  todosData.push(newTodo);
+
+  await writeFile("todos.json", JSON.stringify(todosData, null, 2));
+
+  return Response.json(todosData);
 }
