969cd2a0703ffc1a8aabaebadd9f5c59defe7e23
nirajkumardangi <niraj8825224435@gmail.com>
2026-04-04 20:28:40 +0530

    Handling PUT (edit) request

--- 04-route-handling/app/todos/[id]/route.js
+++ 04-route-handling/app/todos/[id]/route.js
@@ -1,4 +1,6 @@
+import { writeFile } from "fs/promises";
 import todos from "../../../todos.json";
+
 
 export async function GET(_, { params }) {
   const { id } = await params;
@@ -15,4 +17,17 @@ export async function GET(_, { params }) {
   }
 
   return Response.json(todo);
+}
+
+export async function PUT(request, { params }) {
+  const editTodoData = await request.json();
+  const { id } = await params;
+  const todoIndex = todos.findIndex((todo) => id === todo.id);
+  const todo = todos[todoIndex];
+
+  const editedTodo = {...todo, ...editTodoData};
+  todos[todoIndex] = editedTodo;
+
+  await writeFile("todos.json", JSON.stringify(todos, null, 2));
+  return Response.json(editedTodo);
 }
