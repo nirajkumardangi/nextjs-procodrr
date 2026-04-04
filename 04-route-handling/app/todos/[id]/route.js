9c9d665dcc0a19b916a9cd0f3c7234c2944d15c8
nirajkumardangi <niraj8825224435@gmail.com>
2026-04-04 17:50:10 +0530

    Dynamic route handling

---
+++ 04-route-handling/app/todos/[id]/route.js
@@ -0,0 +1,18 @@
+import todos from "../../../todos.json";
+
+export async function GET(_, { params }) {
+  const { id } = await params;
+
+  const todo = todos.find((todo) => id === todo.id.toString());
+
+  if (!todo) {
+    return Response.json(
+      { error: "Todo not found!" },
+      {
+        status: 404,
+      },
+    );
+  }
+
+  return Response.json(todo);
+}
